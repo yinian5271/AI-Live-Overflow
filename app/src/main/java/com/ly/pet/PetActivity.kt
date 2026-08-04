7674b73b994c76fd8882cc6d22281a7a3d457ffb
yinian5271 <yinian5271@users.noreply.github.com>
2026-08-04 14:09:40 +0000

    B方案：仅加一行启动悬浮窗服务，其余100%原版

--- app/src/main/java/com/ly/pet/PetActivity.kt
+++ app/src/main/java/com/ly/pet/PetActivity.kt
@@ -1,6 +1,7 @@
 package com.ly.pet
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
@@ -8,6 +9,9 @@ import android.widget.TextView
 class PetActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 【唯一改动】启动悬浮窗服务，让桌宠出现
+        startService(Intent(this, OverlayService::class.java))
+        // 以下是原版测试页
         val tv = TextView(this)
         tv.text = "桌宠测试"
         tv.setBackgroundColor(Color.RED)
